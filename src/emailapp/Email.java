5f907711f289f962a5a136fe475490bbbdc10628
Ahmet Nakisci <nakiscia@gmail.com>
2019-12-19 00:46:22 +0300

    Initial commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,18 @@
+package emailapp;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String altarnateEmail;
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+}
---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,9 @@
+package emailapp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
